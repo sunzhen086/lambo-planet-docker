cf9894f5c032123a5049667dfee3f05ef868defd
孙振 <stomz@163.com>
2017-11-13 09:36:20 +0800

    增加mysql镜像

--- lambo-planet-mysql/setup/privileges.sql
+++ lambo-planet-mysql/setup/privileges.sql
@@ -1,4 +1,4 @@
 use mysql;
-update user set password=password('root') where user='root';
+update mysql.user set authentication_string=password('root') where user='root' ;
 -- 这一条命令一定要有：
 flush privileges;
